3a92b0d3aaca8aaff17c9c50c77938734b2a93f7
monikabu <mna.burdzy@gmail.com>
2014-11-07 15:54:38 +0100

    wow, amazing

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,7 +2,7 @@ class TrailsController < ApplicationController
   #Excon.defaults[:uri_parser] = Addressable::URI
 
   def index
-    @trail = Trail.last
+    @trails = Trail.all
   end
 
 
@@ -20,18 +20,28 @@ class TrailsController < ApplicationController
       end_point_street: @trail.end_point_street,
       end_point_number: @trail.end_point_number
     }
-    # @car_trip_duration = Trail.trip_duration(trail_params)
-    # get_weather_stats(trail_params)
-    # if @main == "Rain"
-    #   @jak = "chujowo, bo pada"
-    #   if @temperature < 20
-    #     @jak + "i pizga"
-    #   end
-    # end
+    @car_trip_duration = Trail.trip_duration(trail_params)
+    get_weather_stats(trail_params)
 
-    # @jak
-    #@bus_trip_duration = bus_trip_duration(trail_params)
+    @main = JSON.parse(@weather_response.body)['weather'].first['main']
+    @temperature = JSON.parse(@weather_response.body)['main'].first.second - 273.15
+    if @main == "Rain"
+      @jak = "chujowo, bo pada"
+      if @temperature < 20
+        @jak = @jak + " i pizga"
+      end
+    else
+      @jak = "czy Ty musisz być takim leniem, napierdalaj na piechotę"
+    end
 
+    @jak
+    bus_trip_duration(trail_params)
+    if @bus_trip_duration < @car_trip_duration
+      @czym = 'dziś niestety musisz się pointegrować z plebsem - wycieczka autobusem będzie szybszą opcją'
+    else
+      @czym = 'nie bądź głupi, jedź autem'
+    end
+    @czym
   end
 
   def create
@@ -45,25 +55,21 @@ class TrailsController < ApplicationController
   end
 
   def bus_trip_duration(trail_params)
-    get_georeferences(trail_params)
-    @response = Excon.get("http://#{trail_params[:start_point_city]}.jakdojade.pl/?fc=#{@s_p_lat}:#{@s_p_ln}&tc=#{@e_p_lat}:@e_p_ln")
-    throw response
+    @bus_trip_response = Excon.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{trail_params[:start_point_number]}+#{trail_params[:start_point_street]}+#{trail_params[:start_point_city]}&destination=#{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}&mode=transit&departure_time=1415369556&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s")
+    @bus_trip_duration = JSON.parse(@bus_trip_response.body)['routes'].first['legs'].first['duration']['text']
   end
 
-  def get_georeferences(trail_params)
-    ##{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}
-    @start_point_response ||= Excon.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{trail_params[:start_point_number]}+#{trail_params[:start_point_street]}+#{trail_params[:start_point_city]}&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s")
-    @end_point_response ||= Excon.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s")
-    @s_p_ln = JSON.parse(@start_point_response.body)['results'].first['geometry']['location']['lng']
-    @s_p_lat = JSON.parse(@start_point_response.body)['results'].first['geometry']['location']['lat']
-    @e_p_ln = JSON.parse(@end_point_response.body)['results'].first['geometry']['location']['lng']
-    @e_p_lat = JSON.parse(@end_point_response.body)['results'].first['geometry']['location']['lat']
-  end
+  # def get_georeferences(trail_params)
+  #  @start_point_response ||= Excon.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{trail_params[:start_point_number]}+#{trail_params[:start_point_street]}+#{trail_params[:start_point_city]}&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s")
+  #   @end_point_response ||= Excon.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s")
+  #   @s_p_ln = JSON.parse(@start_point_response.body)['results'].first['geometry']['location']['lng']
+  #   @s_p_lat = JSON.parse(@start_point_response.body)['results'].first['geometry']['location']['lat']
+  #   @e_p_ln = JSON.parse(@end_point_response.body)['results'].first['geometry']['location']['lng']
+  #   @e_p_lat = JSON.parse(@end_point_response.body)['results'].first['geometry']['location']['lat']
+  # end
 
   def get_weather_stats(trail_params)
-    @response ||= Excon.get("http://api.openweathermap.org/data/2.5/weather?q=#{trail_params[:start_point_city]}")
-    @main = JSON.parse(@response.body)['weather'].first['main']
-    @temperature = JSON.parse(@response.body)['main'].first.second - 273.15
+    @weather_response = Excon.get("http://api.openweathermap.org/data/2.5/weather?q=#{trail_params[:start_point_city]}")
   end
 
   private
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,6 +1,6 @@
 class Trail < ActiveRecord::Base
   def self.trip_duration(trail_params)
-    response = Excon.get "https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{trail_params[:start_point_number]}+#{trail_params[:start_point_street]}+#{trail_params[:start_point_city]}&destinations=#{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}&mode=driving&language=en-EN&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s"
-    JSON.parse(response.body)['rows'].first['elements'].first['duration']['text']
+    car_trip_response = Excon.get "https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{trail_params[:start_point_number]}+#{trail_params[:start_point_street]}+#{trail_params[:start_point_city]}&destinations=#{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}&mode=driving&language=en-EN&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s"
+    JSON.parse(car_trip_response.body)['rows'].first['elements'].first['duration']['text']
   end
 end
