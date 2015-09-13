65233309c5cf8f2668fbc814b52307d900f67968
monikabu <mna.burdzy@gmail.com>
2015-09-13 15:32:54 +0100

    annotate

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: trails
+#
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  start_point_city   :string(255)
+#  start_point_street :string(255)
+#  start_point_number :string(255)
+#  end_point_city     :string(255)
+#  end_point_street   :string(255)
+#  end_point_number   :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class Trail < ActiveRecord::Base
   def self.trip_duration(trail_params)
     car_trip_response = Excon.get "https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{trail_params[:start_point_number]}+#{trail_params[:start_point_street]}+#{trail_params[:start_point_city]}&destinations=#{trail_params[:end_point_number]}+#{trail_params[:end_point_street]}+#{trail_params[:end_point_city]}&mode=driving&language=en-EN&key=AIzaSyBeilkitrb2rF6u0GYvrbdPhM9qtWgC0_s"
