2bb33c52514cdd130f45d2fda06d7b6e0d9e4fac
monikabu <mna.burdzy@gmail.com>
2015-09-13 17:07:30 +0100

    weather conditions

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -5,7 +5,6 @@ class TrailsController < ApplicationController
     @trails = Trail.all
   end
 
-
   def new
     @trail = Trail.new
   end
@@ -13,17 +12,7 @@ class TrailsController < ApplicationController
   def show
     @car_trip_duration = Trips::Car.duration(trail)
     @bus_trip_duration = Trips::Bus.duration(trail)
-    get_weather_stats(trail)
-
-    @main = JSON.parse(@weather_response.body)['weather'].first['main']
-    @temperature = JSON.parse(@weather_response.body)['main'].first.second - 273.15
-    if @main == "Rain" || @temperature < 15
-      @jak = "przy chujowej pogodzie"
-    else
-      @jak = "czy Ty musisz być takim leniem, napierdalaj na piechotę"
-    end
-
-    @jak
+    @weather_conditions = Weather.conditions(trail)
   end
 
   def create
@@ -34,10 +23,6 @@ class TrailsController < ApplicationController
     else
       render :new
     end
-  end
-
-  def get_weather_stats(trail_params)
-    @weather_response = Excon.get("http://api.openweathermap.org/data/2.5/weather?q=#{trail.start_point_city}")
   end
 
   private
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,3 +1,26 @@
-class Weather < ActiveRecord::Base
+class Weather
+  KELVIN_TO_CELSIUS_DIFF = 273.15
 
+  def self.conditions(trail)
+    response = parse(Excon.get("#{api_base_url}?q=#{trail.start_point_city}"))
+    main = response['weather'].first['main']
+    temperature = response['main'].first.second - KELVIN_TO_CELSIUS_DIFF
+    evaluate_conditions(main, temperature)
+  end
+
+  def self.api_base_url
+    "http://api.openweathermap.org/data/2.5/weather"
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body)
+  end
+
+  def self.evaluate_conditions(main, temperature)
+    if main == "Rain" || temperature < 15
+      I18n.t('weather.poor')
+    else
+      I18n.t('weather.good')
+    end
+  end
 end
